77e118616d8e659d8b2d6416cbcdf568be744729
RhythmGC <tomorinaoamv@gmail.com>
2026-07-03 06:32:18 +0700

    feat: rename Neovim colorscheme to ba and add ba/inir aliases to Fish

---
+++ .config/nvim/colors/ba.lua
@@ -0,0 +1,2 @@
+-- ba.lua — colorscheme alias forwarding to the dynamic inir theme
+vim.cmd("colorscheme inir")
--- .config/nvim/lua/plugins/neovim.lua
+++ .config/nvim/lua/plugins/neovim.lua
@@ -8,7 +8,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "inir",
+      colorscheme = "ba",
     },
   },
 }
