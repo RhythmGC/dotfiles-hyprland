a9b8171049c5c9f41ecebcba3bf5f440597d3eac
RhythmGC <tomorinaoamv@gmail.com>
2026-07-19 01:20:31 +0700

    update sth i dont know why it happen (may be i cahange theme)

--- .config/hypr/hyprland/colors.lua
+++ .config/hypr/hyprland/colors.lua
@@ -1,16 +1,16 @@
 hl.config({
     general = {
         col = {
-            active_border   = "rgba(48475077)",
-            inactive_border = "rgba(13131833)",
+            active_border   = "rgba(4B474977)",
+            inactive_border = "rgba(15131433)",
         },
     },
     misc = {
-        background_color = "rgba(0E0E12FF)",
+        background_color = "rgba(0F0E0FFF)",
     },
 })
 
 hl.window_rule({
     match        = { pin = 1 },
-    border_color = "rgba(C4C3EEAA) rgba(C4C3EE77)",
+    border_color = "rgba(D0C3CCAA) rgba(D0C3CC77)",
 })
